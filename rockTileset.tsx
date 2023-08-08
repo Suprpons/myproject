c58b2792903bd2edc4807cab9b6f6bdb261fe670
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2023-08-08 18:46:21 +0200

    firstCommit

---
+++ rockTileset.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="rockTileset" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="assets/NewSprite73.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="11" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="something red" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="something green" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="something blue" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="2,1,3,1,3,1,2,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
